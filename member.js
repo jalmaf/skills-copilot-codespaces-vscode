d2e2a836e88a80f7017aa5eb7bde240dbedbc5d4
jalmaf <129436152+jalmaf@users.noreply.github.com>
2023-12-26 22:51:40 +0000

    view multiple suggestions

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var skills = ['JavaScript', 'ReactJS', 'React Native'];
+  console.log(skills);
+  skills.push('NodeJS');
+  console.log(skills);
+}
+
+// is skills in global scope?
